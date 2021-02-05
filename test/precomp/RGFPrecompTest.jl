27e6fe0afc8f2c4385dc46ad6a10a80d7445c091
Dan Padilha <dpad@g.ecc.u-tokyo.ac.jp>
2021-02-05 13:08:28 +0900

    Improve test comments.

--- test/precomp/RGFPrecompTest.jl
+++ test/precomp/RGFPrecompTest.jl
@@ -3,13 +3,16 @@ module RGFPrecompTest
     RuntimeGeneratedFunctions.init(@__MODULE__)
     
     z = 100
+
+    # Both f and f2 should be kept in the current module's cache
     f = @RuntimeGeneratedFunction(:((x,y)->x+y+z))
-    f2 = RuntimeGeneratedFunction(@__MODULE__, :((x,y)->x-y+z))
+    f2 = @RuntimeGeneratedFunction(@__MODULE__, :((x,y)->x-y+z))
 
     function build_RGF(ex, mod=@__MODULE__)
-        # This function is used to test out a consumer module (i.e. a module
-        # that calls this module's function) in the RGFPrecompConsumerTest.
-        RuntimeGeneratedFunction(mod, ex)
+        # This function is used to test out a consumer module (e.g. a
+        # user-defined module that calls this module's function).
+        # See the RGFPrecompConsumerTest.
+        @RuntimeGeneratedFunction(mod, ex)
     end
 
     module Submodule
@@ -17,9 +20,11 @@ module RGFPrecompTest
         RuntimeGeneratedFunctions.init(@__MODULE__)
 
         z = 200
+
+        # f should exist in the submodule's cache.
         f = @RuntimeGeneratedFunction(:((x,y)->x*y+z))
 
-        # Define a version in the parent scope (i.e. using the parent module's "z")
-        f2 = RuntimeGeneratedFunction(parentmodule(@__MODULE__), :((x,y)->x/y+z))  
+        # f2 should exist in the parent module (i.e. using the parent module's "z")
+        f2 = @RuntimeGeneratedFunction(parentmodule(@__MODULE__), :((x,y)->x/y+z))  
     end
 end
